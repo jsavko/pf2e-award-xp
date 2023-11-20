c4c55d61c77f90a58f7a5bb2eeef10148ae2241a
jsavko <james@geekwebservices.com>
2023-11-20 12:32:55 -0800

    0.1.1
    Updated `openDialog` method to allow passing of options as an object
    Updated AwardXP Dialog window for custom descriptions.

--- pf2e-awardxp.js
+++ pf2e-awardxp.js
@@ -8,7 +8,7 @@ Hooks.on('preDeleteCombat', (combat,html,id) => {
         combat.combatants.filter(c => c.actor.type === "hazard").map(c => c.actor.system.details.level.value),
         {}
     )
-    game.pf2e_awardxp.openDialog(pcs, calulatedXP.xpPerPlayer, 'Encounter (' + calulatedXP.rating.charAt(0).toUpperCase() +  calulatedXP.rating.slice(1) + ')' )
+    game.pf2e_awardxp.openDialog({actors: pcs, award: calulatedXP.xpPerPlayer, description: 'Encounter (' + calulatedXP.rating.charAt(0).toUpperCase() +  calulatedXP.rating.slice(1) + ')' })
 })
 
 
@@ -18,21 +18,35 @@ Hooks.once("init", async () => {
 });
 
 
-function pf2e_awardxp_dialog(pcs, award, desc) {
-    let selected
-    if (award == undefined) {award = ''}
-    if (!!desc) {
-        selected = `<option selected value="${desc}" data-xp="${award}"> ${desc} ${award} exp</option>`;
+  /**
+   * Open dialog and award exp to Actors. May pass an object: 
+   * @param {object | null} [options] Additional specific data keys/values which override the defaults
+   *                                  actor: array of actors | defaults to the pf2 party
+   *                                  award: XP value to award as INT | defaults 1
+   *                                  description: Text description to display in award message | Defaults "Custom" 
+   */
+
+function pf2e_awardxp_dialog(options={}){
+    let selected;
+    
+    if (options.award == undefined) { options.award = '';}
+    if (!!options.description) {
+        selected = `<option selected value="${options.description}" data-xp="${options.award}"> ${options.description} ${options.award} exp</option>`;
+    }
+    
+    if (!options.actors) {
+        options.actors = game.actors.party.members;
     }
 
     new Dialog({
         title: "Award Party XP",
         content: `
+            <p><b>Recipients</b>: ${options.actors.map(a => a.name ).join(', ')}<br/><br/></p>
             <b>Select encounter difficulty or enter custom amount.</b>
             <form>
             <div class="form-group">
             <label>Award for</label>
-            <select id="award-type">
+            <select id="award-type" style="width: 245px !important;">
                 <option value="Custom">Custom</option>
                 ${selected}
                 <option value="Encounter (Trivial)" data-xp="40">Encounter (Trivial) 40 exp</option>
@@ -47,7 +61,11 @@ function pf2e_awardxp_dialog(pcs, award, desc) {
             </div>
             <div class="form-group">
             <label>Award</label>
-            <input type="text" inputmode="numeric" pattern="\d*" id="custom-xp-amount" value="${award}">
+            <input type="text" style="width: 245px !important;" inputmode="numeric" pattern="\d*"  placeholder="1" id="custom-xp-amount" value="${options.award}">
+            </div>
+            <div class="pf2e_awardxp_description form-group">
+            <label>Description</label>
+            <input type="text" style="width: 245px !important;" inputmode="text" id="custom-description" placeholder="Custom Description" value="">
             </div>
             </form>
             `,
@@ -57,9 +75,10 @@ function pf2e_awardxp_dialog(pcs, award, desc) {
                 label: "Confirm",
                 callback: async (html) => {
                     let type = html.find('[id=award-type]')[0].value;
-                    let xp_gain = parseInt(html.find('[id=custom-xp-amount]')[0].value);
-                    pcs.forEach(async a => await a.update({'system.details.xp.value': xp_gain+a.system.details.xp.value}))
-                    ChatMessage.create({content: `<strong>The Party gained ${xp_gain} XP for ${type}.</strong><br/>Recipients: ${pcs.map(a => a.name).join(', ')}<br/>`})
+                    let xp_gain = parseInt(html.find('[id=custom-xp-amount]')[0].value)|| 1;
+                    if (type == "Custom") type = html.find('[id=custom-description]')[0].value || "Custom";
+                    options.actors.forEach(async a => await a.update({'system.details.xp.value': xp_gain+a.system.details.xp.value}))
+                    ChatMessage.create({content: `<strong>The Party gained ${xp_gain} XP for ${type}.</strong><br/>Recipients: ${options.actors.map(a => a.name).join(', ')}<br/>`})
                 }
             },
             two: {
@@ -69,14 +88,22 @@ function pf2e_awardxp_dialog(pcs, award, desc) {
         },
         default: "Cancel",
         render: (html) =>  {
+            if (html.find('[id=award-type] :selected').text() != "Custom"){
+                $(".pf2e_awardxp_description").css("visibility", "hidden");
+            }
             html.find('[id=award-type]').on( "change", function() {
                 html.find('[id=custom-xp-amount]')[0].value = this.selectedOptions[0].getAttribute("data-xp");
+                if (this.selectedOptions[0].value == "Custom"){
+                    $(".pf2e_awardxp_description").css("visibility", "visible");
+                } else { 
+                    $(".pf2e_awardxp_description").css("visibility", "hidden");
+                }
               } );
         },
 
     }).render(true);
-    
 }
+
 
 
 
