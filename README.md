87faa8f4044bef1bb2a0e1fb85f5e095704a4261
jsavko <james@geekwebservices.com>
2023-11-21 08:25:50 -0800

    Kingdom Exp

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  
 Open an XP award dialog after combat prefilled with XP Value of the previous combat to award all PCs who were part of the combat. 
 
-![XPwindow](https://github.com/jsavko/pf2e-award-xp/assets/192591/44815fd9-279b-45e7-a81b-509f8c29e31b)
+![XPwindow](https://github.com/jsavko/pf2e-award-xp/assets/192591/af5c7a4b-1594-4d39-b4e5-29466fa7b962)
--- pf2e-awardxp.js
+++ pf2e-awardxp.js
@@ -14,7 +14,10 @@ Hooks.on('preDeleteCombat', (combat,html,id) => {
 
 Hooks.once("init", async () => {
     console.log('PF2E Award XP Init')
-    game.pf2e_awardxp = {openDialog: pf2e_awardxp_dialog}
+    game.pf2e_awardxp = {openDialog: pf2e_awardxp_dialog_player,
+                        openPlayerDialog: pf2e_awardxp_dialog_player,
+                        openKingdomDialog: pf2e_awardxp_dialog_kingdom
+                        }
 });
 
 
@@ -26,7 +29,7 @@ Hooks.once("init", async () => {
    *                                  description: Text description to display in award message | Defaults "Custom" 
    */
 
-function pf2e_awardxp_dialog(options={}){
+function pf2e_awardxp_dialog_player(options={}){
     let selected;
     
     if (options.award == undefined) { options.award = '';}
@@ -78,7 +81,10 @@ function pf2e_awardxp_dialog(options={}){
                     let xp_gain = parseInt(html.find('[id=custom-xp-amount]')[0].value)|| 1;
                     if (type == "Custom") type = html.find('[id=custom-description]')[0].value || "Custom";
                     options.actors.forEach(async a => await a.update({'system.details.xp.value': xp_gain+a.system.details.xp.value}))
-                    ChatMessage.create({content: `<strong>The Party gained ${xp_gain} XP for ${type}.</strong><br/>Recipients: ${options.actors.map(a => a.name).join(', ')}<br/>`})
+                    //ChatMessage.create({content: `<strong>The Party gained ${xp_gain} XP for ${type}.</strong><br/>Recipients: ${options.actors.map(a => a.name).join(', ')}<br/>`})
+                    ChatMessage.create({content: `
+                    <div style="float:left; margin:10px 10px 10px 10px"> <img style="border:none" src="modules/pf2e-award-xp/assets/exp64.png" width="45"></div><div style="margin-top:15px"><strong>The Party gained  ${xp_gain} XP for ${type}.</strong><br/><p>Recipients: ${options.actors.map(a => a.name).join(', ')}<br/></p></div><br/>`
+                    })
                 }
             },
             two: {
@@ -106,5 +112,101 @@ function pf2e_awardxp_dialog(options={}){
 
 
 
+
+
+
+function pf2e_awardxp_dialog_kingdom(options = {}) {
+    //game.actors.party.campaign
+    if (!game.actors.party.campaign.active) {
+        ui.notifications.error("You can not award Kingdom Experience before the kingdom is founded.");
+        return
+    }
+
+    let selected;
+    
+    if (options.award == undefined) { options.award = '';}
+    if (!!options.description) {
+        selected = `<option selected value="${options.description}" data-xp="${options.award}"> ${options.description} ${options.award} exp</option>`;
+    }
+
+    new Dialog({
+        title: "Award Kingdom XP",
+        content: `
+            <b>Select milestone or enter custom amount.</b>
+            <form>
+            <div class="form-group">
+            <label>Award for</label>
+            <select id="award-type" style="width: 245px !important;">
+                <option value="Custom">Custom</option>
+                ${selected}
+                <option data-xp="10">Activity: Claim a hex</option>
+                <option data-xp="30">Activity: Kingdom Event</option>
+                <option data-xp="1">Surplus RP XP Award</option>
+                <option data-xp="40">Milestone: Claim your first Landmark</option>
+                <option data-xp="40">Milestone: Claim your first Refuge</option>
+                <option data-xp="40">Milestone: Establish your first village</option>
+                <option data-xp="40" >Milestone: Reach kingdom Size 10</option>
+                <option data-xp="60">Milestone: Establish diplomatic relationsp</option>
+                <option data-xp="60">Milestone: Expand a village into your first town</option>
+                <option data-xp="60">Milestone: All eight leadership roles are assigned</option>
+                <option data-xp="60">Milestone: Reach kingdom Size 25</option>
+                <option data-xp="80">Milestone: Establish your first trade agreement</option>
+                <option data-xp="80">Milestone: Expand a town into your first city</option>
+                <option data-xp="80">Milestone: Reach kingdom Size 50</option>
+                <option data-xp="80">Milestone: Spend 100 RP during a Kingdom turn</option>
+                <option data-xp="120">Milestone: Expand a city into your first metropolis</option>
+                <option data-xp="120">Milestone: Reach kingdom Size 100</option>
+            </select>
+            </div>
+            <div class="form-group">
+            <label>Award</label>
+            <input type="text" style="width: 245px !important;" inputmode="numeric" pattern="\d*"  placeholder="1" id="custom-xp-amount" value="${options.award}">
+            </div>
+            <div class="pf2e_awardxp_description form-group">
+            <label>Description</label>
+            <input type="text" style="width: 245px !important;" inputmode="text" id="custom-description" placeholder="Custom Description" value="">
+            </div>
+            </form>
+            `,
+        buttons: {
+            one: {
+                icon: '<i class="fas fa-check"></i>',
+                label: "Confirm",
+                callback: async (html) => {
+                    let type = html.find('[id=award-type]')[0].value;
+                    let xp_gain = parseInt(html.find('[id=custom-xp-amount]')[0].value)|| 1;
+                    if (type == "Custom") type = html.find('[id=custom-description]')[0].value || "Custom";
+                    //options.actors.forEach(async a => await a.update({'system.details.xp.value': xp_gain+a.system.details.xp.value}))
+                    await game.actors.party.campaign.update({'xp.value': xp_gain + game.actors.party.campaign.xp.value});
+                    //ChatMessage.create({content: `<strong>The Kingdom of ${game.actors.party.campaign.name} gained ${xp_gain} XP for ${type}.</strong><br/>`})
+                    ChatMessage.create({content: `
+                    <div style="float:left; margin:10px 10px 10px 10px"> <img style="border:none" src="modules/pf2e-award-xp/assets/exp64.png" width="45"></div><div style="margin-top:15px"><strong>The Kingdom of ${game.actors.party.campaign.name} gained ${xp_gain} XP for ${type}.</strong><br/></div><br/>`
+                    })
+                
+                }
+            },
+            two: {
+                icon: '<i class="fas fa-times"></i>',
+                label: "Cancel",
+            }
+        },
+        default: "Cancel",
+        render: (html) =>  {
+            if (html.find('[id=award-type] :selected').text() != "Custom"){
+                $(".pf2e_awardxp_description").css("visibility", "hidden");
+            }
+            html.find('[id=award-type]').on( "change", function() {
+                html.find('[id=custom-xp-amount]')[0].value = this.selectedOptions[0].getAttribute("data-xp");
+                if (this.selectedOptions[0].value == "Custom"){
+                    $(".pf2e_awardxp_description").css("visibility", "visible");
+                } else { 
+                    $(".pf2e_awardxp_description").css("visibility", "hidden");
+                }
+              } );
+        },
+
+    }).render(true);
+
+}
 
 
