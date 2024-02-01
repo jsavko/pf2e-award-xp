30aebc46caeb6ac78361a4fdc32648d7c2b09b09
jsavko <james@geekwebservices.com>
2024-02-01 12:23:57 -0800

    Major Refactor

--- pf2e-awardxp.js
+++ pf2e-awardxp.js
@@ -1,3 +1,7 @@
+/* -------------------------------------------- */
+/*  Hooks                                       */
+/* -------------------------------------------- */
+
 Hooks.on('preDeleteCombat', (combat,html,id) => {
     if (!game.user.isGM) return
     const pcs = combat.combatants.filter(c => c.actor.type==='character').map(c => c.actor)
@@ -8,7 +12,8 @@ Hooks.on('preDeleteCombat', (combat,html,id) => {
         combat.combatants.filter(c => c.actor.type === "hazard").map(c => c.actor.system.details.level.value),
         {}
     )
-    game.pf2e_awardxp.openDialog({actors: pcs, award: calulatedXP.xpPerPlayer, description: 'Encounter (' + calulatedXP.rating.charAt(0).toUpperCase() +  calulatedXP.rating.slice(1) + ')' })
+    const award = new game.pf2e_awardxp.Award(null,{destinations:pcs, description:'Encounter (' + calulatedXP.rating.charAt(0).toUpperCase() +  calulatedXP.rating.slice(1) + ')', xp:calulatedXP.xpPerPlayer});
+    award.render(true);
 })
 
 
@@ -16,9 +21,13 @@ Hooks.once("init", async () => {
     console.log('PF2E Award XP Init')
     game.pf2e_awardxp = {openDialog: pf2e_awardxp_dialog_player,
                         openPlayerDialog: pf2e_awardxp_dialog_player,
-                        openKingdomDialog: pf2e_awardxp_dialog_kingdom
+                        openKingdomDialog: pf2e_awardxp_dialog_kingdom,
+                        Award: Award
                         }
+    registerCustomEnrichers()
 });
+
+Hooks.on("chatMessage", (app, message, data) => game.pf2e_awardxp.Award.chatMessage(message));
 
 
   /**
@@ -28,6 +37,124 @@ Hooks.once("init", async () => {
    *                                  award: XP value to award as INT | defaults 1
    *                                  description: Text description to display in award message | Defaults "Custom" 
    */
+
+
+
+export function registerCustomEnrichers() {
+CONFIG.TextEditor.enrichers.push({
+    pattern: /\[\[\/(?<type>award) (?<config>[^\]]+)]](?:{(?<label>[^}]+)})?/gi,
+    enricher: enrichAward
+})
+
+//document.body.addEventListener("click", awardAction);
+}
+
+
+
+/* -------------------------------------------- */
+/*  Enrichers                                   */
+/* -------------------------------------------- */
+
+/**
+ * Enrich an award block displaying amounts for each part granted with a GM-control for awarding to the party.
+ * @param {object} config              Configuration data.
+ * @param {string} [label]             Optional label to replace default text.
+ * @param {EnrichmentOptions} options  Options provided to customize text enrichment.
+ * @returns {HTMLElement|null}         An HTML link if the check could be built, otherwise null.
+ */
+
+function parseConfig(match) {
+    const config = { _config: match, values: [] };
+    for ( const part of match.match(/(?:[^\s"]+|"[^"]*")+/g) ) {
+      if ( !part ) continue;
+      const [key, value] = part.split("=");
+      const valueLower = value?.toLowerCase();
+      if ( value === undefined ) config.values.push(key.replace(/(^"|"$)/g, ""));
+      else if ( ["true", "false"].includes(valueLower) ) config[key] = valueLower === "true";
+      else if ( Number.isNumeric(value) ) config[key] = Number(value);
+      else config[key] = value.replace(/(^"|"$)/g, "");
+    }
+    return config;
+  }
+
+
+async function enrichAward(match, options) {
+    console.log('enrich')
+    let { type, config, label } = match.groups;
+    config = parseConfig(config);
+    config._input = match[0];
+   console.log(type)
+   console.log(config)
+   console.log(label)
+    return null;
+  }
+
+  async function enrichAwards(config, label, options) {
+    const command = config._config;
+    let parsed;
+    try {
+      parsed = Award.parseAwardCommand(command);
+    } catch(err) {
+      console.warn(err.message);
+      return null;
+    }
+  
+    const block = document.createElement("span");
+    block.classList.add("award-block", "dnd5e2");
+    block.dataset.awardCommand = command;
+  
+    const entries = [];
+    for ( let [key, amount] of Object.entries(parsed.currency) ) {
+      const label = CONFIG.DND5E.currencies[key].label;
+      amount = Number.isNumeric(amount) ? formatNumber(amount) : amount;
+      entries.push(`
+        <span class="award-entry">
+          ${amount} <i class="currency ${key}" data-tooltip="${label}" aria-label="${label}"></i>
+        </span>
+      `);
+    }
+    if ( parsed.xp ) entries.push(`
+      <span class="award-entry">
+        ${formatNumber(parsed.xp)} ${game.i18n.localize("DND5E.ExperiencePointsAbbr")}
+      </span>
+    `);
+  
+    let award = game.i18n.getListFormatter({ type: "unit" }).format(entries);
+    if ( parsed.each ) award = game.i18n.format("EDITOR.DND5E.Inline.AwardEach", { award });
+  
+    block.innerHTML += `
+      ${award}
+      <a class="award-link" data-action="awardRequest">
+        <i class="fa-solid fa-trophy"></i> ${label ?? game.i18n.localize("DND5E.Award.Action")}
+      </a>
+    `;
+  
+    return block;
+  }
+  
+  
+  /* -------------------------------------------- */
+
+
+/* -------------------------------------------- */
+/*  Actions                                     */
+/* -------------------------------------------- */
+
+/**
+ * Forward clicks on award requests to the Award application.
+ * @param {Event} event  The click event triggering the action.
+ * @returns {Promise|void}
+
+async function awardAction(event) {
+    const target = event.target.closest('[data-action="awardRequest"]');
+    const command = target?.closest("[data-award-command]")?.dataset.awardCommand;
+    if ( !command ) return;
+    event.stopPropagation();
+    Award.handleAward(command);
+  }
+  
+ */
+
 
 function pf2e_awardxp_dialog_player(options={}){
     let selected;
@@ -210,3 +337,148 @@ function pf2e_awardxp_dialog_kingdom(options = {}) {
 }
 
 
+class Award extends FormApplication {
+
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+          classes: ["pf2e", "award", "dialog","pf2eawardxp"],
+          template: "modules/pf2e-award-xp/templates/apps/award.hbs",
+          title: "PF2EAXP.Award.Title",
+          width: 400,
+          height: "auto",
+          currency: null,
+          xp: null,
+          type: null,
+          description: null,
+          destinations: [],
+        });
+      }
+
+      getData(options={}) {
+        const context = super.getData(options);
+        context.xp = this.options.xp ?? 0;
+        context.description = this.options.description ?? null;
+        context.destinations = this.options.destinations.length > 0 ? this.options.destinations : game.actors.party.members;
+        return context;
+      }
+
+    /** @inheritdoc */
+    async _updateObject(event, formData) {
+        const data = foundry.utils.expandObject(formData);
+        if(data['award-type'] != "Custom") {data.description = data['award-type'];}
+        const destinations = []
+        for (const actor in data.destination){ 
+            if (data.destination[actor] == true) destinations.push(actor)
+        }
+        this.close();
+        await this.constructor.awardXP(data.xp, destinations)
+        //this.constructor.displayAwardMessages(data.xp, data.description,destinations);
+    }
+    
+
+    static async awardXP(amount, destinations){
+        if ( !amount || !destinations.length ) return;
+        for ( const destination of destinations ) {
+            const actor = game.actors.get(destination);
+            await actor.update({'system.details.xp.value': actor.system.details.xp.value + amount})
+        }
+    }
+
+
+  /* -------------------------------------------- */
+  /*  Event Handling                              */
+  /* -------------------------------------------- */
+
+  /** @inheritDoc */
+  activateListeners(html) {
+    super.activateListeners(html);
+    this._validateForm();
+
+      html.find('[name=award-type]').on( "load", function() {
+        html.find('[name=xp]')[0].value = this.selectedOptions[0].getAttribute("data-xp");
+        if (this.selectedOptions[0].value == "Custom"){
+            $(".pf2e_awardxp_description").css("visibility", "visible");
+        } else { 
+            $(".pf2e_awardxp_description").css("visibility", "hidden");
+        }
+      } );
+
+  }
+
+  /* -------------------------------------------- */
+
+  /** @inheritDoc */
+  _onChangeInput(event) {
+    super._onChangeInput(event);
+    this._validateForm();
+  }
+
+
+
+  _validateForm() {
+    const data = foundry.utils.expandObject(this._getSubmitData());
+    let valid = true;
+    //if ( !filteredKeys(data.amount ?? {}).length && !data.xp ) valid = false;
+    //if ( !filteredKeys(data.destination ?? {}).length ) valid = false;
+    this.form.querySelector('button[name="transfer"]').disabled = !valid;
+  }
+
+
+
+  /* -------------------------------------------- */
+  /*  Chat Command                                */
+  /* -------------------------------------------- */
+
+  /**
+   * Regular expression used to match the /award command in chat messages.
+   * @type {RegExp}
+   */
+  static COMMAND_PATTERN = new RegExp(/^\/award(?:\s|$)/i);
+
+  /* -------------------------------------------- */
+
+  /**
+   * Regular expression used to split currency & xp values from their labels.
+   * @type {RegExp}
+   */
+  //static VALUE_PATTERN = new RegExp(/^(.+?)(\D+)$/);
+  static VALUE_PATTERN = new RegExp(/^(\d+)(.*)/);
+
+  /* -------------------------------------------- */
+
+  /**
+   * Use the `chatMessage` hook to determine if an award command was typed.
+   * @param {string} message   Text of the message being posted.
+   * @returns {boolean|void}   Returns `false` to prevent the message from continuing to parse.
+   */
+  static chatMessage(message) {
+    if ( !this.COMMAND_PATTERN.test(message) ) return;
+    this.handleAward(message);
+    return false;
+  }
+
+  static async handleAward(message) {
+    if ( !game.user.isGM ) {
+        ui.notifications.error("PF2EAXP.Award.NotGMError", { localize: true });
+        return;
+      }
+
+      try {
+        const { xp, description } = this.parseAwardCommand(message);
+        const award = new game.pf2e_awardxp.Award(null,{xp:xp, description:description});
+        award.render(true);
+
+      } catch(err) {
+        ui.notifications.warn(err.message);
+      }
+
+  }
+
+  static parseAwardCommand(message) {
+    const command = message.replace(this.COMMAND_PATTERN, "");
+    let [full, xp, description] = command.match(this.VALUE_PATTERN) ?? [];
+    return { xp, description };
+  }
+
+
+}
